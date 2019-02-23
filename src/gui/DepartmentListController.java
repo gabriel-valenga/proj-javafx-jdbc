80e47fceb8081520971858653e374899af15f3f0
gabriel-valenga <you@example.com>
2019-02-23 11:04:36 -0300

    Department Service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService service;
+	
 	@FXML 
 	private TableView<Department> tableViewDepartment;
 	
@@ -32,6 +38,7 @@ public class DepartmentListController implements Initializable {
       //TO-DO
 	}
 	
+	private ObservableList<Department> obsList;
 	
 	
 	@Override
@@ -46,15 +53,22 @@ public class DepartmentListController implements Initializable {
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
 		
 		Stage stage = (Stage) Main.getMainScene().getWindow();
-//		tableViewDepartment.setMinHeight(stage.getHeight());
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
-		tableViewDepartment.prefWidthProperty().bind(stage.widthProperty());
-
-
-		//tableViewDepartment.setMinHeight(stage.heightProperty().doubleValue());
-		//tableViewDepartment.prefWidthProperty().bind(stage.widthProperty());
-		
-		
+		tableViewDepartment.prefWidthProperty().bind(stage.widthProperty());		
 	}
 
+	
+	public void setDepartmentService (DepartmentService service) {
+		this.service = service;
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service is null");
+		}
+		List <Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+		
+	}
 }
---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,16 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+	
+	public List<Department> findAll() {
+		List<Department> list = new ArrayList<>();
+		
+		return list;
+	}
+
+}
